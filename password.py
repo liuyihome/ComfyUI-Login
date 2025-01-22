195f488a2d04bb5fdea7faa53945ae3e334d067d
jerric <15605156008@163.com>
2025-01-22 20:25:56 +0800

    fix: 固定密码加盐

--- password.py
+++ password.py
@@ -133,7 +133,7 @@ async def login_handler(request):
             return web.HTTPFound('/login?wrong_password=1')
     else:
         # New user setup
-        salt = b"default"
+        salt = b'$2b$12$1234567890123456789012'
         hashed_password = bcrypt.hashpw(password_input, salt)
         with open(password_path, "wb") as file:
             file.write(hashed_password + b'\n' + username_input.encode('utf-8'))
